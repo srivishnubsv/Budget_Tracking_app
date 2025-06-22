ec886612dd97c0e00500a74432679812fb632e12
Sri Vishnu <srivishnubsv@gmail.com>
2025-06-22 10:56:41 +0530

    Modified frontend

--- frontend/budget-tracking-app/src/utils/apiPaths.js
+++
@@ -1,9 +0,0 @@
-export const API_BASE_URL = "https://api.example.com"; // Replace with your actual API base URL
-
-export const API_ENDPOINTS = {
-  LOGIN: `${API_BASE_URL}/auth/login`,
-  SIGNUP: `${API_BASE_URL}/auth/signup`,
-  GET_INCOME: `${API_BASE_URL}/dashboard/income`,
-  GET_EXPENSE: `${API_BASE_URL}/dashboard/expense`,
-  GET_DASHBOARD: `${API_BASE_URL}/dashboard`,
-};
